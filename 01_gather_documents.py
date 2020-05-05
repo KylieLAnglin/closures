19a34fe25f007a9e4737426a7badea826b446ee3
KylieLAnglin <kylie@anglin.com>
2020-05-04 20:55:17 -0400

    collect top three google hits for district + Spring Break

--- 01_gather_documents.py
+++ 01_gather_documents.py
@@ -16,9 +16,9 @@ from library import start
 # https://rptsvr1.tea.texas.gov/perfreport/tapr/2019/download/DownloadData.html
 districts = pd.read_csv(os.path.join(start.dir_path + 'DREF.dat'))
 districts = districts[['DISTRICT', 'DISTNAME', 'CNTYNAME', 'DFLCHART']]
-districts = districts.loc[districts['DFLCHART'] == "N"] # drop charters
-rename_var_dict = {'DISTRICT': 'distcode', 'DISTNAME': 'district', 
-'CNTYNAME': 'county'}
+districts = districts.loc[districts['DFLCHART'] == "N"]  # drop charters
+rename_var_dict = {'DISTRICT': 'distcode', 'DISTNAME': 'district',
+                   'CNTYNAME': 'county'}
 districts = districts[list(rename_var_dict.keys())]
 districts = districts.rename(index=str, columns=rename_var_dict)
 districts.head()
@@ -28,3 +28,20 @@ districts.head()
 # Random sample of 15 districts from proof of concept
 districts = districts.sample(5, random_state=672)
 districts
+
+# %% Run Google search for webpages likely to contain spring break info
+# Note that when we scale this we may need to use a different search engine
+link1 = []
+link2 = []
+link3 = []
+for district in districts.district:
+    query1 = district + " Spring Break"
+    print(query1)
+    urls = []
+    for j in googlesearch.search(query1, tld="co.in", num=2, stop=3, pause=2):
+        urls.append(j)
+        print(j)
+    link1.append(urls[0])
+    link2.append(urls[1])
+    link3.append(urls[2])
+link1
